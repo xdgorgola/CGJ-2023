bcab684f76909dd4383ec1bcb7d5eefd94a202ca
LuisAParraE <38765324+LuisAParraE@users.noreply.github.com>
2023-02-01 23:00:34 -0400

    #Plant Base Code
    Codigo Base de La planta

---
+++ Assets/Scripts/PlantComponent.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlantComponent : MonoBehaviour
+{   
+
+    //VALORES MAXIMOS
+    [SerializeField] private float maxAgua;
+    [SerializeField] private float maxNutrientes;
+    [SerializeField] private float maxVida;
+
+    //VALORES ACTUALES
+    [SerializeField] private float agua;
+    [SerializeField] private float nutrientes;
+    [SerializeField] private float vida;
+
+    //Events
+    [SerializeField] private UnityEvent OnFlowerDeath = new UnityEvent();
+    [SerializeField] private UnityEvent OnNutrientsCap = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxAgua = 20f;
+        maxNutrientes = 10f;
+        maxVida = 10f;
+        agua = 10f;
+        nutrientes = 0f;
+        vida = 10f;
+    }
+
+    public void ChangeWaterCount(float cost)
+    {   
+        if (agua + cost<= maxAgua && agua + cost >= 0)
+        {
+            agua = agua + cost;
+        }
+        
+    }
+
+    public void UpdateLife(float cost)
+    {
+        if (vida + cost <= maxVida && vida + cost > 0)
+        {
+            vida = vida + cost;
+        }
+        else if (vida + cost == 0)
+        {
+            OnFlowerDeath.Invoke();
+        }
+
+    }
+
+    public void UpdateNutrients(float cost)
+    {
+        if (nutrientes + cost < maxNutrientes && nutrientes + cost >= 0)
+        {
+            nutrientes = nutrientes + cost;
+        }
+        else if (nutrientes + cost == maxNutrientes)
+        {
+            //TRIGGER PARA CONSEGUIR MEJORA
+            OnNutrientsCap.Invoke();
+            nutrientes = 0f;
+        }
+
+    }
+
+    public void ReceiveCard(GameCards card)
+    {
+        CardEffects effect = card.CardEffect;
+        switch (effect)
+        {
+            case CardEffects.GainWater:
+                ChangeWaterCount(card.GetParams()["quant"]);
+                return;
+
+            case CardEffects.GainNutrient:
+                UpdateNutrients(card.GetParams()["quant"]);
+                ChangeWaterCount(-1);
+                return;
+
+        }
+    }
+}
