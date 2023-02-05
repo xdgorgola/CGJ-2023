06b2ab3f8fb84b85c49025a14df030f2b5926cca
xdgorgola <pedro18fe@gmail.com>
2023-02-05 13:58:11 -0400

    bg music

---
+++ Assets/Scripts/AudioChanger.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AudioChanger : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _newSource = null;
+    [SerializeField]
+    private AudioSource _oldSource = null;
+
+
+    [SerializeField]
+    private AudioClip _springClip = null;
+    [SerializeField]
+    private AudioClip _summerClip = null;
+    [SerializeField]
+    private AudioClip _fallClip = null;
+    [SerializeField]
+    private AudioClip _winterClip = null;
+
+
+    private void Start()
+    {
+        _oldSource.Play();
+        _oldSource.loop = true;
+        _newSource.loop = true;
+    }
+
+
+    public void ChangeAudioSeason(Season newSeason)
+    {
+        AudioClip targetClip = newSeason switch
+        {
+            Season.Spring => _springClip,
+            Season.Summer => _summerClip,
+            Season.Fall => _fallClip,
+            Season.Winter => _winterClip,
+            _ => null
+        };
+
+        StartCoroutine(FadeAudiosRoutine(targetClip));
+    }
+
+
+    private IEnumerator FadeAudiosRoutine(AudioClip targetClip)
+    {
+        yield return _oldSource.DOFade(0f, 1f).WaitForCompletion();
+
+        _oldSource.Stop();
+        _newSource.clip = targetClip;
+        _newSource.Play();
+        yield return _newSource.DOFade(1f, 1f).WaitForCompletion();
+
+        AudioSource tmp = _oldSource;
+        _oldSource = _newSource;
+        _newSource = tmp;
+    }
+}
